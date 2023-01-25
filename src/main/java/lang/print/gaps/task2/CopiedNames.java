ef676b07d85271bbd69f35c27d3618a69abd80df
sadyrin <sadyrin@gmail.com>
2023-01-25 19:29:35 +0200

    fix issues in tasks "Module #3. Task #2. Key Points"

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,15 +2,13 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int[] variables = {
-                              0
-                            , 2000
-                            , 1000
-                            , 4000
-                            , 3000
-                            , 5000
-                            , 7000
-                            , 90000
-                        };
+        int variable0 = 0;
+        int variable1 = 2000;
+        int variable2 = 4000;
+        int variable3 = 5000;
+        int variable4 = 7000;
+        int value0 = 1000;
+        int value1 = 3000;
+        int value2 = 90000;
     }
 }
